a255a396217de8beac98794935cafb6af35a809b
Artian <artianrika@gmail.com>
2024-08-05 19:54:45 +0200

    wip: translation, some ui improvements

--- offset_print/src/Components/Navbar.js
+++ offset_print/src/Components/Navbar.js
@@ -2,8 +2,18 @@ import "./Navbar.css"
 import logo from "../logo_op_bw.png"
 import {Link} from "react-router-dom";
 
+import albanianFlag from "../albanianFlag.jpg"
+import englishFlag from "../englishFlag.jpg"
+import macedonianFlag from "../macedonianFlag.jpg"
+import {useTranslation} from "react-i18next";
 
 const Navbar = () => {
+
+    const [t, i18n] = useTranslation("global");
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang)
+    }
+
     return (
         <div className={"navbarDiv"}>
             <nav className={"navbar"}>
@@ -12,19 +22,19 @@ const Navbar = () => {
                 </Link>
 
                 <ul>
-                    <Link to={"/home"}><li>Home</li></Link>
-                    <Link to={"/products"}><li>Products</li></Link>
-                    <Link to={"/about-us"}><li>About Us</li></Link>
-                    <Link to={"/contact"}><li>Contact</li></Link>
+                    <Link to={"/home"}><li>{t("navbar.home")}</li></Link>
+                    <Link to={"/products"}><li>{t("navbar.products")}</li></Link>
+                    <Link to={"/about-us"}><li>{t("navbar.about-us")}</li></Link>
+                    <Link to={"/contact"}><li>{t("navbar.contact")}</li></Link>
                 </ul>
 
 
                 <div className={"languageSelectorDiv"}>
-                    <select className={"languageSelector"}>
-                        <option value="en">English</option>
-                        <option value="al">Shqip</option>
-                        <option value="en">Македонски</option>
-                    </select>
+                    <div className={"flagContainer"}>
+                        <img src={englishFlag} onClick={() => changeLanguage("en")}/>
+                        <img src={albanianFlag} onClick={() => changeLanguage("sq")}/>
+                        <img src={macedonianFlag} onClick={() => changeLanguage("mk")}/>
+                    </div>
                 </div>
             </nav>
         </div>
--- offset_print/src/index.js
+++ offset_print/src/index.js
@@ -5,15 +5,45 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+import global_en from "./translations/en/global.json"
+import global_sq from "./translations/sq/global.json"
+import global_mk from "./translations/mk/global.json"
+import i18next from "i18next"
+import {I18nextProvider, initReactI18next} from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+    interpolation: {escapeValue: false},
+    lng: "en",
+    resources:{
+        en: {
+            global: global_en
+        },
+        sq: {
+            global: global_sq
+        },
+        mk:{
+            global: global_mk
+        }
+    },
+        fallbackLng: "en"
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-      <BrowserRouter>
+      <I18nextProvider i18n={i18next}>
+          <BrowserRouter>
              <App />
-      </BrowserRouter>
+          </BrowserRouter>
+      </I18nextProvider>
 
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
